a2471e4887db9e0cd2c9584196b3a4bd315c5ca3
Chris-Grant301 <72807122+Chris-Grant301@users.noreply.github.com>
2021-01-19 11:28:46 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 while True:
     print("sound level:" + input.sound_level())
-    if input.sound_level() > 75:
-        music.play_melody("G A B G A B C B C D D C B A G D", 120)
+    if input.sound_level() > 150:
+        music.wawawawaa.play_until_done()
     else:
         light.clear()
--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 while (true) {
     console.log("sound level:" + input.soundLevel())
-    if (input.soundLevel() > 75) {
-        music.playMelody("G A B G A B C B C D D C B A G D", 120)
+    if (input.soundLevel() > 150) {
+        music.wawawawaa.playUntilDone()
     } else {
         light.clear()
     }
